5d87e1e1495168b259b7cd20807e67876a4f6f19
dieow <danielsitarz@gmail.com>
2016-12-20 22:37:25 +0100

    Console log msg

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log('msg: ' + msg);
   });
 });
 
 http.listen(process.env.PORT || 5000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:' + process.env.PORT || 5000);
 });
