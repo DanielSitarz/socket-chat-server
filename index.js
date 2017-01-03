9a993e7646c36dd368aaf2fd0c2eab3a52c36d81
dieow <danielsitarz@gmail.com>
2017-01-03 21:45:49 +0100

    Server doesnt emit dc or conn to other rooms

--- index.js
+++ index.js
@@ -6,21 +6,22 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){        
-
-  userConnectedEvent(socket); 
-
-  socket.on('disconnect', function(data){    
-    userDisconnectedEvent(socket);
-  });
-
-  //Client emits current room name
+io.on('connection', function(socket){            
+  //First what client do is to emit current room name
   socket.on('enter room', function(roomName){
+    socket.roomName = roomName;
+
     socket.join(roomName);
+
+    userConnectedEvent(socket); 
 
      socket.on('chat message', function(data){    
        socket.broadcast.to(roomName).emit('chat message', data);
      }); 
+
+     socket.on('disconnect', function(data){    
+       userDisconnectedEvent(socket);
+     });
   });
 });
 
@@ -29,7 +30,7 @@ http.listen(process.env.PORT || 5000, function(){
 });
 
 var userConnectedEvent = function(socket){
-  socket.broadcast.emit('chat message', {
+  socket.broadcast.to(socket.roomName).emit('chat message', {
       key: (new Date()).getTime(),
       sender: "Chat",
       content: "User connected.",
@@ -39,7 +40,7 @@ var userConnectedEvent = function(socket){
 }
 
 var userDisconnectedEvent = function(socket){
-  socket.broadcast.emit('chat message', {
+  socket.broadcast.to(socket.roomName).emit('chat message', {
       key: (new Date()).getTime(),
       sender: "Chat",
       content: "User disconnected.",
