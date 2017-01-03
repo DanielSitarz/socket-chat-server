d194a588fb4ff433f3fb9b4b972c09ed868c4e94
dieow <danielsitarz@gmail.com>
2017-01-03 22:08:14 +0100

    Name change is not broadcasted

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ io.on('connection', function(socket){
 
     socket.join(socket.chatData.roomName);
 
-    sendMessageFromServer(socket, socket.chatData.userName + " connected.");
+    sendMessageFromServer(socket, socket.chatData.userName + " connected.", true);
 
      socket.on('chat message', function(data){    
        socket.broadcast.to(socket.chatData.roomName).emit('chat message', data);
@@ -28,11 +28,11 @@ io.on('connection', function(socket){
      socket.on('name change', function(newName){    
        var oldName = socket.chatData.userName;
        socket.chatData.userName = newName;
-       sendMessageFromServer(socket, oldName + " changes name to " + newName + ".");
+       sendMessageFromServer(socket, oldName + " changes name to " + newName + ".", false);
      }); 
 
      socket.on('disconnect', function(data){    
-       sendMessageFromServer(socket, socket.chatData.userName + " disconnected.");
+       sendMessageFromServer(socket, socket.chatData.userName + " disconnected.", true);
      });
   });
 });
@@ -41,12 +41,18 @@ http.listen(process.env.PORT || 5000, function(){
   console.log('listening on *:' + process.env.PORT || 5000);
 });
 
-var sendMessageFromServer = function(socket, content){
-  socket.broadcast.to(socket.chatData.roomName).emit('chat message', {
+var sendMessageFromServer = function(socket, content, isBroadcasting){
+  var msg = {
       key: (new Date()).getTime(),
       sender: "Chat",
       content: content,
       power: 0,
       isServerMsg: true
-  })
+  };
+
+  if(isBroadcasting){
+    socket.broadcast.to(socket.chatData.roomName).emit('chat message', msg);
+  }else{
+    socket.to(socket.chatData.roomName).emit('chat message', msg);
+  }
 }
