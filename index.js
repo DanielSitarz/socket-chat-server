d84b2c376f69ec6958fa9551e4a7b48276145076
dieow <danielsitarz@gmail.com>
2017-01-07 22:24:26 +0100

    on use enter the room event and standard style

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require('express')()
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
-io.on('connection', function(socket){            
-  //First what client do is emit current chat data
+io.on('connection', function (socket) {
+  // First what client do is emit current chat data
   /**
    *  chatData = {
    *    userName,
    *    rommName
-   *  } 
+   *  }
    */
-  socket.on('enter room', function(chatData){
-    socket.chatData = chatData;
+  socket.on('enter room', function (chatData) {
+    socket.chatData = chatData
 
-    socket.join(socket.chatData.roomName);
+    socket.join(socket.chatData.roomName)
 
-    sendMessageFromServer(socket, socket.chatData.userName + " connected.");
+    socket.broadcast.to(socket.chatData.roomName).emit('user enter the room', socket.chatData.userName)
 
-     socket.on('chat message', function(data){    
-       socket.broadcast.to(socket.chatData.roomName).emit('chat message', data);
-     }); 
+    socket.on('chat message', function (data) {
+      socket.broadcast.to(socket.chatData.roomName).emit('chat message', data)
+    })
 
-     socket.on('is typing', function(who){    
-       socket.broadcast.to(socket.chatData.roomName).emit('is typing', who);
-     }); 
-     socket.on('stopped typing', function(who){    
-       socket.broadcast.to(socket.chatData.roomName).emit('stopped typing', who);
-     }); 
+    socket.on('is typing', function (who) {
+      socket.broadcast.to(socket.chatData.roomName).emit('is typing', who)
+    })
+    socket.on('stopped typing', function (who) {
+      socket.broadcast.to(socket.chatData.roomName).emit('stopped typing', who)
+    })
 
-     socket.on('name change', function(newName){    
-       var oldName = socket.chatData.userName;
-       socket.chatData.userName = newName;
-       sendMessageFromServer(socket, oldName + " changes name to " + newName + ".");
-     }); 
+    socket.on('name change', function (newName) {
+      var oldName = socket.chatData.userName
+      socket.chatData.userName = newName
+      sendMessageFromServer(socket, oldName + ' changes name to ' + newName + '.')
+    })
 
-     socket.on('disconnect', function(data){    
-       sendMessageFromServer(socket, socket.chatData.userName + " disconnected.");
-     });
-  });
-});
+    socket.on('disconnect', function (data) {
+      sendMessageFromServer(socket, socket.chatData.userName + ' disconnected.')
+    })
+  })
+})
 
-http.listen(process.env.PORT || 5000, function(){
-  console.log('listening on *:' + process.env.PORT || 5000);
-});
+http.listen(process.env.PORT || 5000, function () {
+  console.log('listening on *:' + process.env.PORT || 5000)
+})
 
-var sendMessageFromServer = function(socket, content){
-  var msg = {            
-      content: content,      
-      isFromServer: true
-  };
+var sendMessageFromServer = function (socket, content) {
+  var msg = {
+    content: content,
+    isFromServer: true
+  }
 
-  socket.broadcast.to(socket.chatData.roomName).emit('chat message', msg);
+  socket.broadcast.to(socket.chatData.roomName).emit('chat message', msg)
 }
